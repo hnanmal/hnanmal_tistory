c95a53e90eeb10cbdd404866fe8713d424e6d083
pjmk007 <pjmk007@gmail.com>
2022-05-03 17:21:07 +0900

    asdf

---
+++ 2022.05.03_C# 반복문/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+//namespace ConsoleApp1
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] arr = { 1, 2, 5, 7, 4, 9, 8, 10, 4, 7, 11 };
+
+//            foreach (int i in arr)
+//                Console.WriteLine("i: {0}", i);
+//        }
+//    }
+//}
+
+
+int[] 배열 = { 1, 2, 5, 7, 4, 9, 8, 10, 4, 7, 11 };
+
+foreach (int i in 배열) 
+    Console.WriteLine("i: {0}", i);
